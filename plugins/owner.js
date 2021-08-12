5c4c3b5e6f20b7ea9cf54ad72d091297051aca76
Safeer07 <88830491+Safeer07@users.noreply.github.com>
2021-08-12 20:41:22 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -25,7 +25,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'Amalser Bot created by *Amal*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'Bot created by *Safeer*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '\n\n---------------------', MessageType.text);
@@ -43,7 +43,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'Amalser Bot created by *Amal*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'Bot created by *Safeer*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '\n\n--------------------', MessageType.text);
