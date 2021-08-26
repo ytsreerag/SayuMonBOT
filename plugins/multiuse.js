00b0ab42d913a9baaf68a30e39c3cb40511d9aea
Safeer07 <88830491+Safeer07@users.noreply.github.com>
2021-08-26 16:22:19 +0530

    Update multiuse.js

--- plugins/multiuse.js
+++ plugins/multiuse.js
@@ -8,7 +8,7 @@ const IG_DESC = "Downloads Image/Video From Instagram"
 
 Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: IG_DESC }, async (message, match) => {
     //if(match[1] == '') return
-    let { data, type } = await instaGram(match[1], 'f3eaf19231f6201c');
+    let { data, type } = await instaGram(match[1], '208c3e1637c946a6');
     //if(type == undefined) return 
     if (type === 'image') { await message.sendMessage(data, MessageType.image, { caption: "Made by safeerbot" }) }
     else if (type === 'video') { await message.sendMessage(data, MessageType.video, { caption: "Made by safeerbot" }) }
